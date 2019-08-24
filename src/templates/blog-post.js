277405f2a15da48c03cebce9830262faa062f13a
RG Aquino <ralphgregoraquino@gmail.com>
2019-08-24 18:27:09 +0800

    Revert component naming

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
 class BlogPostTemplate extends React.Component {
