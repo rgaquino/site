ad72dac60baa3a5176506b22eb0827d441f56232
RG Aquino <ralphgregoraquino@gmail.com>
2019-08-24 19:28:04 +0800

    RS-0001: Refactor

---
+++ gatsby/node/createPages.js
@@ -0,0 +1,50 @@
+const path = require(`path`);
+
+module.exports = async ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    const blogPost = path.resolve(`./src/templates/blog-post.jsx`)
+    const result = await graphql(
+        `
+        {
+            allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            limit: 1000
+            ) {
+            edges {
+                node {
+                fields {
+                    slug
+                }
+                frontmatter {
+                    title
+                }
+                }
+            }
+            }
+        }
+        `
+    )
+
+    if (result.errors) {
+        throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach((post, index) => {
+        const previous = index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+        },
+        })
+    })
+}
