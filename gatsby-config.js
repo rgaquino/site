a643f4050975eabf4bd74a2f58e0749828f30b65
RG Aquino <ralphgregoraquino@gmail.com>
2019-08-29 14:18:00 +0800

    RS-0002: Modify Google Analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        respectDNT: false,
       },
     },
     `gatsby-plugin-feed`,
