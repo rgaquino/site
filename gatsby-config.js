aa04c0020e5aedf80117e606ea29f8bea6c73e5b
RG Aquino <ralphgregoraquino@gmail.com>
2019-09-08 15:08:30 +0800

    RL-0022: Migrate Medium posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1000,
+              linkImagesToOriginal: false,
             },
           },
           {
